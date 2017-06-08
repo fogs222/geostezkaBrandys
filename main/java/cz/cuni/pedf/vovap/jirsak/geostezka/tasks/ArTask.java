1aaf16b62c5624b7d899a3a534863426cc0a34ba
Tomas Jerab <t.jerab@gmail.com>
2017-06-08 07:31:31 +0200

    Prepare for AR task

---
+++ main/java/cz/cuni/pedf/vovap/jirsak/geostezka/tasks/ArTask.java
@@ -0,0 +1,18 @@
+package cz.cuni.pedf.vovap.jirsak.geostezka.tasks;
+
+import cz.cuni.pedf.vovap.jirsak.geostezka.utils.Task;
+
+/**
+ * Created by tomason on 01.06.2017.
+ */
+
+public class ArTask extends Task {
+
+	public ArTask(int id, int typ, String nazev, String zadani) {
+		super(id, typ, nazev, zadani);
+	}
+
+	public void setTargets() {
+
+	}
+}
